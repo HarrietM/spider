5c447a79d5a323c33132176b516170e2d61f8cd4
HarrietM <hsemckinnon@gmail.com>
2015-06-21 19:04:24 +1200

    Get Links from hackernews

--- spider.js
+++ spider.js
@@ -1,6 +1,20 @@
 var request = require('request');
-request('https://news.ycombinator.com', function(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body)
+var cheerio = require('cheerio');
+var async = require('async');
+
+var spider = function() {
+  var self = this;
+  request('https://news.ycombinator.com', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      self.getLinks(body)
+    }
+  })
+
+  this.getLinks = function(html) {
+    var $ = cheerio.load(html);
+    var links = $('a');
+    console.log(links)
   }
-})
+}
+
+spider();
